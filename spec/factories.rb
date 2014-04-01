d969c5f333add2d0e0bc0c5cf0caacb70446c2e0
ms_rezaei <ms.rezaei@example.com>
2014-04-01 13:09:03 -0700

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Mohammad Sadegh Rezaei"
+    email    "ms.rezaei@ut.ac.ir"
+    password "yamahdy"
+    password_confirmation "yamahdy"
+  end
+end
